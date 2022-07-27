fae69479ead89ff9729f5e34c1a9f9d2f24dfc78
kamilkhalili <kamilkhal@gmail.com>
2022-07-27 09:27:33 +0800

    Update flowmeter.py

--- flowmeter.py
+++ flowmeter.py
@@ -19,14 +19,15 @@ GPIO.add_event_detect(FLOW_SENSOR_GPIO, GPIO.FALLING, callback=countPulse)
 
 async def flowcount():
     global count, start_counter
+    while True:
         start_counter = 1
-        await.asyncio.sleep(1)
+        await asyncio.sleep(1)
         start_counter = 0
         flow = (count / 7.5) # Pulse frequency (Hz) = 7.5Q, Q is flow rate in L/min.
         print("The flow is: %.3f Liter/min" % (flow))
         #publish.single("/Garden.Pi/WaterFlow", flow, hostname=MQTT_SERVER)
         count = 0
-        await.asyncio.sleep(5)
+        await asyncio.sleep(5)
 
 
 loop= asyncio.get_event_loop()

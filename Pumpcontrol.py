77dd4c5d308e4c00b7765acab3fd33225d07bb7d
kamilkhalili <kamilkhal@gmail.com>
2022-08-03 11:57:06 +0800

    Integration

--- Pumpcontrol.py
+++ Pumpcontrol.py
@@ -1,6 +1,7 @@
 
 import RPi.GPIO as GPIO
 import time
+import vdata
 
 GPIO.cleanup
 
--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import asyncio
 from time import sleep
 import asynccontrol
 import flowmeter
+import Relay
+import Pumpcontrol
 
+GPIO.setwarnings(False)			#disable warnings
 
 try:
-    asyncio.ensure_future(asynccontrol.pumpcontrol())
-    #asyncio.ensure_future(flowmeter.flowcount()) #Flow meter measurement
+    asyncio.ensure_future(flowmeter.flowcount()) #Flow meter measurement
+    asyncio.ensure_future(Relay.main_pump()) #Pump timer control
+    asyncio.ensure_future(Pumpcontrol.pump_1())
     asynccontrol.loop.run_forever()
 
 except KeyboardInterrupt:
@@ -14,6 +18,3 @@ except KeyboardInterrupt:
 finally:
     print("Closing loop")
     asynccontrol.loop.close()
-    
-    #test again again
-    #test test

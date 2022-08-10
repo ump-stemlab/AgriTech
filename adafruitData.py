d77da391feaa51ef1f83e7877e2ace1106a97a1a
kamilkhalili <kamilkhal@gmail.com>
2022-08-10 12:15:57 +0800

    Updated adafruit io functionality
    Only data from bme280 is available

--- adafruitData.py
+++ adafruitData.py
@@ -5,30 +5,34 @@ import vdata
 # import Adafruit IO REST client
 from Adafruit_IO import Client, Feed, RequestError
 
-# loop timeout, in seconds.
-LOOP_DELAY = 10
 
 #Adafuit IO Username and Key
 ADAFRUIT_IO_USERNAME = 'umpstemlab'
-ADAFRUIT_IO_KEY = 'Yaio_zCup0319HtRZTly8U34SwC8oNNkD'
+ADAFRUIT_IO_KEY = 'aio_zCup0319HtRZTly8U34SwC8oNNkD'
 aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 
 try:
-    temperature_feed = aio.feeds('Ambient_temp')
-    pressure_feed = aio.feeds('Air_pressure')
+    temperature_feed = aio.feeds('ambient')
+    pressure_feed = aio.feeds('pressure')
     humidity_feed = aio.feeds('humidity')
 
 except RequestError:
-    temperature_feed = aio.create_feed(Feed(name='Ambient_temp'))
-    pressure_feed = aio.create_feed(Feed(name='Air_pressure'))
+    temperature_feed = aio.create_feed(Feed(name='ambient'))
+    pressure_feed = aio.create_feed(Feed(name='pressure'))
     humidity_feed = aio.create_feed(Feed(name='humidity'))
 
-time.sleep(1)
 
+    
 async def Send_data():
     while True:
         aio.send(temperature_feed.key, vdata.ambient_temperature)
+        print('Temperature' + str(vdata.ambient_temperature))
         aio.send(pressure_feed.key, int(vdata.pressure))
+        print('Pressure' + str(int(vdata.pressure)))
         aio.send(humidity_feed.key, int(vdata.humidity))
-        await asyncio.wait(30)
+        print('Pressure' + str(vdata.humidity))
+        print('Data sent')
+        await asyncio.sleep(10)
+        
+loop= asyncio.get_event_loop()
 
--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import asyncio
 from time import sleep
 import flowmeter
 import BME280test
+import adafruitData
 
 
 try:
 #     asyncio.ensure_future(flowmeter.flowcount()) #Flow meter measurement
     asyncio.ensure_future(BME280test.checkTemp())
+    asyncio.ensure_future(adafruitData.Send_data())
 #     flowmeter.loop.run_forever()
     BME280test.loop.run_forever()
-
+    adafruitData.loop.run_forever()
     
 
 except KeyboardInterrupt:
@@ -17,4 +19,5 @@ except KeyboardInterrupt:
 finally:
     print("Closing loop")
     BME280test.loop.close()
+    adafruitData.loop.close()
     
